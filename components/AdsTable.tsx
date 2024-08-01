46f173a6a21afd677bd7ed0f89a64ff0ebd65eab
Devesh <purohitdev08@gmail.com>
2024-08-01 13:19:33 +0530

    Assignment completed

---
+++ components/AdsTable.tsx
@@ -0,0 +1,79 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import data2 from "@/data/data2.json";
+import Image from "next/image";
+import { HiOutlineArrowLeft, HiOutlineArrowRight } from "react-icons/hi";
+
+const AdsTable = () => {
+  const adsData = data2.data.results;
+  return (
+    <div className="border  shadow-md rounded-md">
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Thumbnail</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Brand</TableHead>
+            <TableHead>Total ad spend</TableHead>
+            <TableHead>Ad spend 30</TableHead>
+            <TableHead>Publish Date</TableHead>
+            <TableHead>Duration</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {adsData.map((video) => (
+            <TableRow key={video.ytVideoId}>
+              <TableCell>
+                <Image
+                  src={video.thumbnail}
+                  height={35}
+                  width={35}
+                  className="rounded-full h-[35px]"
+                  alt="thumbnail"
+                />
+              </TableCell>
+              <TableCell>{video.title}</TableCell>
+              <TableCell>{video.brandName}</TableCell>
+              <TableCell>{video.totalSpend || 0}</TableCell>
+              <TableCell>{video.last30Days || 0}</TableCell>
+              <TableCell>{video.publishedAt}</TableCell>
+              <TableCell>{video.duration}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className="flex justify-between items-center border-t p-3">
+        <div className="flex flex-1 justify-center items-center space-x-4">
+          <HiOutlineArrowLeft className="px-2 py-1 border text-3xl rounded-md w-10 cursor-pointer" />
+          <span>1</span>
+          <HiOutlineArrowRight className="px-2 py-1 border text-3xl rounded-md w-10 cursor-pointer" />
+        </div>
+        <div className="border py-1.5 rounded-md px-2">
+          <select>
+            <option className="p-2" value="1">
+              1
+            </option>
+            <option className="p-2" value="2">
+              2
+            </option>
+            <option className="p-2" value="3">
+              3
+            </option>
+            <option className="p-2" value="4">
+              4
+            </option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdsTable;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,103 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import data1 from "@/data/data1.json";
+import { HiOutlineStar } from "react-icons/hi";
+import { HiOutlineQuestionMarkCircle, HiOutlineShare} from "react-icons/hi2";
+import AdsTable from "@/components/AdsTable";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const companyData = data1.data;
+  const company = companyData.company;
+  const companyRanks = companyData.ranks;
+  const top5Countries = companyData.top5Countries;
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center bg-gray-50 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <section className="bg-slate-300 w-full">
+        <div className="flex flex-col justify-center items-center space-y-4 py-4">
+          <h1 className="text-2xl font-bold">{company.legalName}</h1>
+          <div className="flex space-x-4">
+            <button className="py-1 px-2 rounded-md border bg-white flex justify-center items-center">
+              <HiOutlineStar className="mr-2"/> Swipe
+            </button>
+            <button className="py-1 px-2 rounded-md border bg-blue-600 text-white flex justify-center items-center">
+              <HiOutlineShare className="mr-2"/> Share
+            </button>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-4 my-4">
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <div className="grid grid-flow-row gap-4 border p-4 rounded-lg shadow-md">
+          <div className="border-b font-semibold">Company Info</div>
+          <div className="text-sm">
+            <span className="font-semibold">Company: </span>
+            {company.legalName}
+          </div>
+          <div className="text-sm">
+            <span className="font-semibold">Year Founded: </span>
+            {company.yearFounded}
+          </div>
+          <div className="text-sm">
+            <span className="font-semibold">Employees: </span>
+            {company.numberOfEmployees}
+          </div>
+          <div className="text-sm">
+            <span className="font-semibold">Headquarters: </span>
+            {company.headquarters}
+          </div>
+          <div className="text-sm">
+            <span className="font-semibold">Category: </span>
+            <span className="text-blue-700 font-medium">Online Video</span>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4">
+        <div className="border p-4 flex flex-col space-y-2 rounded-lg shadow-md">
+            <span className="text-xs flex items-center">AD spent 365 <HiOutlineQuestionMarkCircle className="ml-1 mb-[1px] text-gray-500 text-sm"/> </span>
+            <span className="font-semibold">
+              ${company.spend.last365Days.toLocaleString()}
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </div>
+          <div className="border p-4 flex flex-col space-y-2 rounded-lg shadow-md">
+            <span className="text-xs flex items-center">Country rank <HiOutlineQuestionMarkCircle className="ml-1 mb-[1px] text-gray-500 text-sm"/> </span>
+            <span className="font-semibold">#{companyRanks.global.rank}</span>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-4">
+          <div className="border p-4 flex flex-col space-y-2 rounded-lg shadow-md">
+            <span className="text-xs flex items-center">Global rank <HiOutlineQuestionMarkCircle className="ml-1 mb-[1px] text-gray-500 text-sm"/> </span>
+            <span className="font-semibold">#{companyRanks.country.rank}</span>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="border p-4 flex flex-col space-y-2 rounded-lg shadow-md">
+            <span className="text-xs flex items-center">Category rank <HiOutlineQuestionMarkCircle className="ml-1 mb-[1px] text-gray-500 text-sm"/> </span>
+            <span className="font-semibold">#{companyRanks.category.rank}</span>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        <div className="grid grid-flow-row gap-4 border p-4 rounded-lg shadow-md">
+          <div className="border-b font-semibold">Campaigns per country</div>
+          {top5Countries.map((country) => (
+            <div key={country.countryId} className="text-xs flex items-center space-x-1">
+              <Image src={country.icon} width={20} height={20} alt="flag" className="mr-1.5"/>
+              {country.countryId}:
+              <span className="font-medium pl-[1px]">
+                {country.count} ({Math.round(country.percentage)})
+              </span>
+            </div>
+          ))}
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="p-4 w-[25rem] md:w-[37rem] lg:w-[70rem]">
+        <AdsTable/>
+      </section>
     </main>
   );
 }
